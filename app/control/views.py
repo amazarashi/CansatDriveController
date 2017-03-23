ec5997d2c082f658f9dd1d6ba2b83442afda480e
suguru <u.sgr.22114w@gmail.com>
2017-03-23 23:56:22 +0900

    add all clear and signal setting

--- app/control/views.py
+++ app/control/views.py
@@ -5,19 +5,27 @@ from django.views.decorators.csrf import csrf_protect
 from django.views.decorators.csrf import csrf_exempt
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
+
+## RIGHT
+## - go(PIN1:ON,PIN2:OFF)
+## - back(PIN1:OFF,PIN2:ON)
 PIN1 = 11
 #p0
 PIN2 = 12
 #p5
+## LEFT
+## - go(PIN3:ON,PIN4:OFF)
+## - back(PIN3:OFF,PIN4:ON)
 PIN3 = 13
 #p3
 PIN4 = 23
 #p4
+
 GPIO.setup(PIN1, GPIO.OUT)
 GPIO.setup(PIN2, GPIO.OUT)
 GPIO.setup(PIN3, GPIO.OUT)
 GPIO.setup(PIN4, GPIO.OUT)
-#
+
 # PIN1_STATUS = False
 # PIN2_STATUS = False
 # PIN3_STATUS = False
@@ -51,10 +59,16 @@ def statusHandler(request):
 def pinHandler(request,command,status):
     print("## command ## : ",command)
     print("## status ## : ",status)
+    ## RIGHT WHEEL
+    ## - go(PIN1:ON,PIN2:OFF)
+    ## - back(PIN1:OFF,PIN2:ON)
+    ## LEFT WHEEL
+    ## - go(PIN3:ON,PIN4:OFF)
+    ## - back(PIN3:OFF,PIN4:ON)
     if command == "forward" and status == "on":
-        PIN1_STATUS = False
+        PIN1_STATUS = True
         PIN2_STATUS = False
-        PIN3_STATUS = False
+        PIN3_STATUS = True
         PIN4_STATUS = False
         GPIO.output(PIN1, PIN1_STATUS)
         GPIO.output(PIN2, PIN2_STATUS)
@@ -71,9 +85,9 @@ def pinHandler(request,command,status):
         GPIO.output(PIN4, PIN4_STATUS)
     elif command == "backward" and status == "on":
         PIN1_STATUS = False
-        PIN2_STATUS = False
+        PIN2_STATUS = True
         PIN3_STATUS = False
-        PIN4_STATUS = False
+        PIN4_STATUS = True
         GPIO.output(PIN1, PIN1_STATUS)
         GPIO.output(PIN2, PIN2_STATUS)
         GPIO.output(PIN3, PIN3_STATUS)
@@ -88,76 +102,28 @@ def pinHandler(request,command,status):
         GPIO.output(PIN3, PIN3_STATUS)
         GPIO.output(PIN4, PIN4_STATUS)
     elif command == "right_go" and status == "on":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
+        PIN1_STATUS = True
         GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "right_go" and status == "off":
         PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
         GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "right_back" and status == "on":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
+        PIN2_STATUS = True
         GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "right_back" and status == "off":
-        PIN1_STATUS = False
         PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
         GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "left_go" and status == "on":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
+        PIN3_STATUS = True
         GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "left_go" and status == "off":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
         PIN3_STATUS = False
-        PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
         GPIO.output(PIN3, PIN3_STATUS)
-        GPIO.output(PIN4, PIN4_STATUS)
     elif command == "left_back" and status == "on":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
-        PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
+        PIN4_STATUS = True
         GPIO.output(PIN4, PIN4_STATUS)
     elif command == "left_back" and status == "off":
-        PIN1_STATUS = False
-        PIN2_STATUS = False
-        PIN3_STATUS = False
         PIN4_STATUS = False
-        GPIO.output(PIN1, PIN1_STATUS)
-        GPIO.output(PIN2, PIN2_STATUS)
-        GPIO.output(PIN3, PIN3_STATUS)
         GPIO.output(PIN4, PIN4_STATUS)
     else:
         PIN1_STATUS = False
